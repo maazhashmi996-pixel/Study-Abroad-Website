5f2048795b0f35c68a1d9e774f0bf6c9f7b3f608
Arooj Mubarik <aroojmubarik12@gmail.com>
2026-07-21 11:45:11 +0500

    Main Branch Code

---
+++ app/home/hero.tsx
@@ -0,0 +1,11 @@
+
+
+export default function hero() {
+  return (
+    <div>
+      <h1>home</h1>
+    </div>
+  )
+}
+
+
---
+++ component/navbar.jsx
@@ -0,0 +1,166 @@
+"use client";
+
+import Link from "next/link";
+import { FaPhoneAlt } from "react-icons/fa";
+import { IoIosArrowDown } from "react-icons/io";
+import logoImg from "../public/images/logo.png";
+
+export default function Navbar() {
+  return (
+    <header className="bg-white py-4 shadow-sm">
+      <div className="max-w-7xl mx-auto flex items-center justify-between">
+        {/* Logo */}
+        <div>
+          <img src={logoImg} alt="logo.png" />
+        </div>
+
+        {/* Navigation */}
+        <nav className="bg-[#43246f] rounded-full px-15 py-2">
+          <ul className="flex items-center gap-3 text-white text-xs ">
+            <li>
+              <Link href="/" className="text-yellow-400">
+                Home
+              </Link>
+            </li>
+
+            <li>
+              <Link href="/about">About</Link>
+            </li>
+
+            <li className="relative group">
+              <button className="flex items-center gap-1">
+                Services
+                <IoIosArrowDown />
+              </button>
+
+              {/* Dropdown */}
+              <div className="absolute top-10 left-0 w-60 bg-[#43246f] text-white opacity-0 invisible group-hover:opacity-100 group-hover:visible duration-300">
+                <Link
+                  href="#"
+                  className="block px-5 py-3 hover:bg-yellow-400 transition-colors duration-500"
+                >
+                  Select Your Program
+                </Link>
+
+                <Link
+                  href="#"
+                  className="block px-5 py-3 hover:bg-yellow-500 transition-colors duration-500"
+                >
+                  Submit Application
+                </Link>
+
+                <Link
+                  href="#"
+                  className="block px-5 py-3 hover:bg-yellow-500 transition-colors duration-500"
+                >
+                  Receive Offer Letter
+                </Link>
+
+                <Link
+                  href="#"
+                  className="block px-5 py-3 hover:bg-yellow-500 transition-colors duration-500"
+                >
+                  Fulfill Conditions
+                </Link>
+
+                <Link
+                  href="#"
+                  className="block px-5 py-3 hover:bg-yellow-500 transition-colors duration-500"
+                >
+                  Pay Tuition Deposit
+                </Link>
+
+                <Link
+                  href="#"
+                  className="block px-5 py-3 hover:bg-yellow-500 transition-colors duration-500"
+                >
+                  Apply For Visa
+                </Link>
+
+                <Link
+                  href="#"
+                  className="block px-5 py-3 hover:bg-yellow-500 transition-colors duration-500"
+                >
+                  Start Your Program
+                </Link>
+              </div>
+            </li>
+
+            <li>
+              <Link href="/blogs">Blogs</Link>
+            </li>
+
+            <li>
+              <Link href="/events">Events</Link>
+            </li>
+
+            <li>
+              <Link href="/offices">Our Offices</Link>
+            </li>
+
+            <li>
+              <Link href="/destinations">Study Destinations</Link>
+            </li>
+
+            <li className="relative group">
+              <button className="flex items-center gap-1">
+                Study Abroad Steps
+                <IoIosArrowDown />
+              </button>
+              <div className="absolute top-10 left-0 w-60 bg-[#43246f] text-white opacity-0 invisible group-hover:opacity-100 group-hover:visible duration-300">
+                <Link
+                  href="#"
+                  className="block px-5 py-3 hover:bg-yellow-500 transition-colors duration-500"
+                >
+                  Select Your Program
+                </Link>
+                <Link
+                  href="#"
+                  className="block px-5 py-3 hover:bg-yellow-500 transition-colors duration-500"
+                >
+                  Select Your Program
+                </Link>
+                <Link
+                  href="#"
+                  className="block px-5 py-3 hover:bg-yellow-500 transition-colors duration-500"
+                >
+                  Select Your Program
+                </Link>
+                <Link
+                  href="#"
+                  className="block px-5 py-3 hover:bg-yellow-500 transition-colors duration-500"
+                >
+                  Select Your Program
+                </Link>
+                <Link
+                  href="#"
+                  className="block px-5 py-3 hover:bg-yellow-500 transition-colors duration-500"
+                >
+                  Select Your Program
+                </Link>
+                <Link
+                  href="#"
+                  className="block px-5 py-3 hover:bg-yellow-500 transition-colors duration-500"
+                >
+                  Select Your Program
+                </Link>
+              </div>
+            </li>
+          </ul>
+        </nav>
+
+        {/* Right Side */}
+        <div className="flex items-center gap-8">
+          <div className="flex items-center gap-2 text-lg">
+            <FaPhoneAlt className="text-orange-500" />
+            +92 345 2066 100
+          </div>
+
+          <button className="bg-[#43246f] text-white rounded-full px-4 py-2 hover:bg-[#5b378f] duration-300">
+            Get Free Consultation
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+}
