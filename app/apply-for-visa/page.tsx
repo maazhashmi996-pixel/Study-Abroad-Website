2c18eee5b8026925f08469709d35704094475aee
Arooj Mubarik <aroojmubarik12@gmail.com>
2026-08-01 10:28:05 +0500

    update theme

---
+++ app/apply-for-visa/page.tsx
@@ -0,0 +1,12 @@
+import ApplyForVisaPage from '@/components/apply for visa/visa1'
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      <ApplyForVisaPage/>
+    </div>
+  )
+}
+
+export default page
